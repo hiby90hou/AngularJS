4e1142ffe735336bb4d9d43c042c4c0c2ab79a46
hiby90hou <hiby.90hou@qq.com>
2017-10-06 23:18:54 +1100

    change react project

--- react/practice/my-app/src/index.js
+++ react/practice/my-app/src/index.js
@@ -1,48 +1,52 @@
-import React from 'react';
+//渲染应用组件
+import './index.css';
+import React from 'react';//引入第三方模块
 import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App';
+//引入App
+import App from './components/App/App.js';//引入自定义模块
+
 // import registerServiceWorker from './registerServiceWorker';
 
 // ReactDOM.render(<App />, document.getElementById('root'));
 // registerServiceWorker();
-ReactDOM.render(
-  <h1>Hello, world!</h1>,
-  document.getElementById('root')
-);
 // ReactDOM.render(
-//   <p>I am an exp</p>,
-//   document.getElementById('exp1')
+//   <h1>Hello, world!</h1>,
+//   document.getElementById('root')
 // );
+// // ReactDOM.render(
+// //   <p>I am an exp</p>,
+// //   document.getElementById('exp1')
+// // );
 
-class RefsTest extends React.Component {
-constructor (props) {
-super(props);
+// class RefsTest extends React.Component {
+// constructor (props) {
+// super(props);
 
-this.showMsg = this.showMsg.bind(this); // 强制给showMsg()绑定this(组件对象)
-this.handleBlur = this.handleBlur.bind(this);
-}
+// this.showMsg = this.showMsg.bind(this); // 强制给showMsg()绑定this(组件对象)
+// this.handleBlur = this.handleBlur.bind(this);
+// }
 
-showMsg() {
-// console.log(this); //this默认是null, 而不组件对象
-const input = this.refs.msg;
-alert(input.value);
-}
+// showMsg() {
+// // console.log(this); //this默认是null, 而不组件对象
+// const input = this.refs.msg;
+// alert(input.value);
+// }
 
-handleBlur(event) {
-const input = event.target;
-alert(input.value);
-}
+// handleBlur(event) {
+// const input = event.target;
+// alert(input.value);
+// }
 
-render () {
-return (
-  <div>
-    <input type="text" ref="msg"/>
-    <button onClick={this.showMsg}>提示输入数据</button>
-    <input type="text" placeholder="失去焦点提示数据" onBlur={this.handleBlur}/>
-  </div>
-);
-}
-}
+// render () {
+// return (
+//   <div>
+//     <input type="text" ref="msg"/>
+//     <button onClick={this.showMsg}>提示输入数据</button>
+//     <input type="text" placeholder="失去焦点提示数据" onBlur={this.handleBlur}/>
+//   </div>
+// );
+// }
+// }
 
-ReactDOM.render(<RefsTest/>, document.getElementById('exp1'));
+// ReactDOM.render(<RefsTest/>, document.getElementById('exp1'));
+ReactDOM.render(<App/>, document.getElementById('root'));
