ab837a5897021639ace3b2c4a0b7d3e4fff22f36
hiby90hou <hiby.90hou@qq.com>
2017-10-30 16:04:37 +1100

    add datepicker

--- react/practice/react-native/AwesomeProject/src/TodoItem/TodoItem.js
+++ react/practice/react-native/AwesomeProject/src/TodoItem/TodoItem.js
@@ -1,18 +1,36 @@
 import React from 'react';
 // import PubSub from 'pubsub-js'
-import {Text, View, CheckBox, Button, StyleSheet} from 'react-native';
+import {Text, View, CheckBox, Button, StyleSheet,DatePickerAndroid} from 'react-native';
 
 class TodoItem extends React.Component {
 	//checkbox状态改变的监听回调函数
-	handleChange = ()=>{
+	handleChange = (async()=>{
 		const {updateTodoChecked,todo}= this.props
 		todo.isDone = !todo.isDone
 		if(todo.isDone){
 			console.log('输入时间');
 			todo.expire = "2017/11/1"
+			try {
+			  const {action, year, month, day} = await DatePickerAndroid.open({
+			    // Use `new Date()` for current date.
+			    // May 25 2020. Month 0 is January.
+			    date: new Date(),
+			    mode:'spinner'		    
+			  });
+			  if (action !== DatePickerAndroid.dismissedAction) {
+			    // Selected year, month (0-11), day
+			    
+			  }
+			  var date = new Date(year,month,day)
+			  var getMonth = parseInt(date.getMonth())+1;
+			  todo.expire = date.getFullYear()+"/"+getMonth+"/"+date.getDate()
+			  console.log(todo.expire);
+			} catch ({code, message}) {
+			  console.warn('Cannot open date picker', message);
+			}
 		}
 		updateTodoChecked()
-	}
+	})
 
 	//点击删除相应
 	deleteTodo = () => {
