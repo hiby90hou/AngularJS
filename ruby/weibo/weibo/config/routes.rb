8feea78d6af7a65fd9974e8e82fe881e655a61fe
darkend <hiby.90hou@qq.com>
2017-11-22 13:33:25 +1100

    reg

--- ruby/weibo/weibo/config/routes.rb
+++ ruby/weibo/weibo/config/routes.rb
@@ -1,5 +1,32 @@
 Rails.application.routes.draw do
-  resources :posts
+  # resources :posts
+	# 一般路由
+  # get 'posts/:id', :to => 'posts#show' 
+
+    # 命名路由
+    # get 'posts/:id', :to => 'posts#show', :as => 'show_post'
+
+
+  # resources :posts, :except => :show
+
+  # 命名路由
+
+  #  resources :posts do
+  #   get 'resent', :on => :collection
+  # end
+
+  # 集合路由 成員路由
+
+  resources :posts do
+    collection do
+      get 'resent'
+    end
+    member do
+      get 'resent'
+    end
+  end
+
+
   # get 'posts/index'
 
   # get 'posts/show'
