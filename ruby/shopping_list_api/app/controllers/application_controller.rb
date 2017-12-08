73559443797607f825d659de13a6469f9689aec0
darkend <hiby.90hou@qq.com>
2017-12-09 10:15:28 +1100

    delete wrong api project

--- ruby/shopping_list_api/app/controllers/application_controller.rb
+++
@@ -1,6 +0,0 @@
-class ApplicationController < ActionController::API
-  def current_user
-        @current_user ||= User.find(session[:email]) if session[:email]
-    end
-     helper_method :current_user
-end
