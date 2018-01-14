d130c76674ef0a49df4092d30e1806e0bd85c2bb
darkend <hiby.90hou@qq.com>
2018-01-14 21:32:01 +1100

    change database structure

--- ruby/fridge_share/fridge_share.sql
+++ ruby/fridge_share/fridge_share.sql
@@ -1,6 +1,7 @@
 -- goods_store_table (item_id, user_id, fridge_id, goods_id, goods_expire_date, goods_quantity)
 -- user_table (user_id, username, email, password)
 -- fridge_table (fridge_id,fridge_models)
+-- fridge_user_relationship (id,fridge_id,user_id,relationship(own=0/share=1))
 -- goods_info (goods_id, goods_name, goods_barcode, recommend_expire_period, category, brand, description, images_url, features, attributes, reviews)
 
 -- CREATE DATABASE fridge_db;
@@ -18,12 +19,21 @@ CREATE TABLE user_table (
 	user_id SERIAL PRIMARY KEY,
 	username VARCHAR(255),
 	email VARCHAR(1024),
-	password VARCHAR(1024)
+	password VARCHAR(1024),
+	fridge_quantity INT
 );
 
 CREATE TABLE fridge_table (
 	fridge_id SERIAL PRIMARY KEY,
-	fridge_models VARCHAR(255)
+	fridge_models VARCHAR(255),
+	fridge_owner_id VARCHAR(255)
+);
+
+CREATE TABLE fridge_user_relationship (
+	id SERIAL PRIMARY KEY,
+	fridge_id VARCHAR(255),
+	user_id VARCHAR(255),
+	relationship INT
 );
 
 CREATE TABLE goods_info (
@@ -38,15 +48,39 @@ CREATE TABLE goods_info (
 	features VARCHAR(1024),
 	attributes VARCHAR(65536),
 	reviews VARCHAR(65536)
-
 );
 
 -- make a new user
 -- INSERT INTO user_table
--- (username, email, password)
--- VALUES('hiby', 'hiby.90hou@gmail.com', '123456');
+-- (username, email, password,fridge_quantity)
+-- VALUES('hiby', 'hiby.90hou@gmail.com', '123456','0');
 
 -- make a new fridge
 -- INSERT INTO fridge_table
--- (fridge_models)
--- VALUES('sony models 1');
+-- (fridge_models,fridge_owner_id)
+-- VALUES('LG','1');
+
+-- store a new item in fridge
+-- INSERT INTO goods_store_table
+-- (user_id,fridge_id,goods_id,goods_expire_date,goods_quantity)
+-- VALUES('1','1','1','2018/02/01','2');
+
+-- record a relationship between fridge and user
+-- INSERT INTO fridge_user_relationship
+-- (fridge_id,user_id,relationship)
+-- VALUES('1','3','0');
+
+-- create a new goods infer
+-- INSERT INTO goods_info
+-- (goods_name, goods_barcode, recommend_expire_period, category, brand, description,image_url,
+-- features,attributes,reviews)
+-- VALUES('Sunrice Brown Med 90 Sec Rice','EAN 9310140001913',
+-- 	'365','quick-food','Sunrice','Sunrice Brown Med 90 Sec Rice very good to eat',
+-- 	'https://images-na.ssl-images-amazon.com/images/I/41HAZ-byqjL.jpg','some features',
+-- 	'{Weight => 110 lbs}','{1 => "good product",2 => "do not like it"}');
+
+-- check a user details
+-- SELECT * FROM user_table WHERE username = 'hiby';
+
+-- check user's owned fridge
+-- SELECT * FROM fridge_table WHERE fridge_owner_id = '1';
--- ruby/fridge_share/main.rb
+++ ruby/fridge_share/main.rb
@@ -1,13 +1,53 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'pg'
 
 get '/' do
   erb :index
 end
 
-get '/user/new' do
+get '/users/new' do
+	@alert = params[:alert_info]
 	erb :signup
 end
 
+post '/users' do
+	if params[:password] == params[:password_confirmation]
+		conn = PG.connect(dbname: 'fridge_db')
+		sql = "INSERT INTO user_table
+						(username, email, password,fridge_quantity)
+						VALUES('#{params[:username]}', '#{params[:email]}', '#{params[:password]}','0');"
+		result = conn.exec(sql)
+		conn.close
+		redirect "/users/#{params[:username]}"
+	else
+		redirect '/users/new?alert_info=Please input same password in password and password confirmation'
+	end
+end
 
+post '/users/search' do
+	redirect "/users/#{params[:username]}"
+end
+
+get '/users/:username' do
+		conn = PG.connect(dbname: 'fridge_db')
+		sql = "SELECT * FROM user_table WHERE username = '#{params[:username]}';"
+		result = conn.exec(sql)
+		conn.close
+
+		@user_name = result[0]['username']
+		@user_id = result[0]['user_id']
+		@fridge_quantity = result[0]['fridge_quantity'].to_i
+
+		if @fridge_quantity > 0
+			conn = PG.connect(dbname: 'fridge_db')
+			sql = "SELECT * FROM fridge_user_relationship WHERE user_id = '#{@user_id}';"
+			@fridge_list = conn.exec(sql2)
+			conn.close
+		end
+
+		# return num = fridge_list[0].to_json
+
+		erb :user_main_page
+end
 
