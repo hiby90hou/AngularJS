6cb438d62d3820a8471019a13473516e1dc29e69
darkend <hiby.90hou@qq.com>
2018-03-06 23:45:11 +1100

    refacting

--- ruby/interview/robot/robot.rb
+++ ruby/interview/robot/robot.rb
@@ -1,128 +1,116 @@
 require "pry"
 class Robot
-	def initialize(x=0,y=0,f=0)
+	attr_accessor :x, :y, :angle
+	def initialize(x=0,y=0,angle=0)
 		@x = x
 		@y = y
-		@f = f
+		@angle = angle
 	end
-
-	def get_x
-		return @x
-	end
-
-	def get_y
-		return @y
-	end
-
-	def get_f
-		return @f
-	end
-
-	def set_x new_x
-		@x = new_x
-	end
-
-	def set_y new_y
-		@y = new_y
-	end
-
-	def set_f new_f
-		@f = new_f
-	end
-
-	# def move
-	# 	can_move = Valid_action.new(@x,@y)
-	# 	if can_move.valid_place? == false
-	# 		return
-	# 	end
-
-	# 	if is_north? && can_move.can_go_north?
-	# 		@y = @y+1
-	# 	elsif is_east? && can_move.can_go_east?
-	# 		@x = @x+1
-	# 	elsif is_south? && can_move.can_go_south?
-	# 		@y = @y-1
-	# 	elsif is_west? && can_move.can_go_west?
-	# 		@x = @x-1
-	# 	end
-	# end
-
-	# def turn(direction)
-	# 	can_turn = Valid_action.new(@x,@y)
-	# 	if !can_turn.valid_place?
-	# 		return
-	# 	end
-	# 	if direction == "left"
-	# 		@f = @f - 90
-	# 	else
-	# 		@f = @f + 90
-	# 	end
-	# end
-
-	# def report
-	# 	return {"x"=>@x,"y"=>@y,"face"=>@f}
-	# end
 end
 
 class Controller
 	def initialize(row=5 ,column=5)
 		@row = row
 		@column = column
+		@interface = Interface.new()
+		@robot = Robot.new()
 	end
 
-	def place_robot(x, y, f)
-		@robot = Robot.new(x, y, f)
+	def main_controller
+		data = @interface.get_data
+		if data == nil
+			return "error: no valid command"
+		end
+
+		first_place_index = data.find_index { |e| e.match( /^PLACE [0-9]+,[0-9]+,(NORTH|SOUTH|EAST|WEST)$/ ) }
+		if first_place_index == nil
+			return "error: user did not place robot on table"
+		end
+
+		valid_data = data.slice(first_place_index,data.length)
+		valid_data.each do |command|
+			case command
+			when /^PLACE [0-9]+,[0-9]+,(NORTH|SOUTH|EAST|WEST)$/
+				command.sub!("PLACE ","")
+				command_arr = command.split(",")
+				place_robot(command_arr[0].to_i,command_arr[1].to_i,command_arr[2])
+			when /^MOVE$/
+				move()
+			when /^LEFT$/
+				turn("left")
+			when /^RIGHT$/
+				turn("right")
+			when /^REPORT$/
+				show_place()
+			end
+		end
 	end
 
-	def move
-		x = @robot.get_x
-		y = @robot.get_y
-		f = @robot.get_f
-
+	def place_robot(x, y, direction)
 		if !valid_place?(x, y)
 			return
 		end
 
-		if show_direction(f) == "NORTH" && can_go_north?(x, y)
-			@robot.set_y(y+1)
-		elsif show_direction(f) == "EAST" && can_go_east?(x, y)
-			@robot.set_x(x+1)
-		elsif show_direction(f) == "SOUTH" && can_go_south?(x, y)
-			@robot.set_y(y-1)
-		elsif show_direction(f) == "WEST" && can_go_west?(x, y)
-			@robot.set_x(x-1)
+		@robot.x = x
+		@robot.y = y
+		@robot.angle = direction_translate(direction)
+	end
+
+	def move
+		x = @robot.x
+		y = @robot.y
+		angle = @robot.angle
+
+		if show_direction(angle) == "NORTH" && can_go_north?(x, y)
+			@robot.y = y+1
+		elsif show_direction(angle) == "EAST" && can_go_east?(x, y)
+			@robot.x = x+1
+		elsif show_direction(angle) == "SOUTH" && can_go_south?(x, y)
+			@robot.y = y-1
+		elsif show_direction(angle) == "WEST" && can_go_west?(x, y)
+			@robot.x = x-1
 		end
 	end
 
 	def turn(direction)
-		x = @robot.get_x
-		y = @robot.get_y
-		f = @robot.get_f
-		
-		if !valid_place?(x, y)
-			return
-		end
+		x = @robot.x
+		y = @robot.y
+		angle = @robot.angle
 
 		if direction == "left"
-			@robot.set_f(f - 90)
+			@robot.angle = angle - 90
 		else
-			@robot.set_f(f + 90)
+			@robot.angle = angle + 90
+		end
+	end
+
+	def direction_translate(direction)
+		case direction.upcase
+		when "NORTH"
+			return 0
+		when "EAST"
+			return 90
+		when "SOUTH"
+			return 180
+		when "WEST"
+			return 270
 		end
 	end
 
 	def show_place
-		direction = show_direction(@robot.get_f)
-		return {"x":@robot.get_x, "y":@robot.get_y, "dir":direction}
+		direction = show_direction(@robot.angle)
+		report_object = {"x":@robot.x, "y":@robot.y, "dir":direction}
+		@interface.report(report_object)
 	end
 
-	def show_direction(f)
-		if f%360 == 0
+	def show_direction(angle)
+		if angle%360 == 0
 			return "NORTH"
-		elsif f%360 == 90
+		elsif angle%360 == 90
 			return "EAST"
-		elsif f%360 == 180
+		elsif angle%360 == 180
 			return "SOUTH"
-		elsif f%360 == 270
+		elsif angle%360 == 270
 			return "WEST"
 		else
 			return "error direction"
@@ -153,51 +141,58 @@ end
 
 class Interface
 
-	def initialize
-		@controller = Controller.new
-	end
-
-	def read_file(fileName = "./test/input.txt")
-		@data = []
-		File.open("./test/input.txt", "r") do |f|
+	def read_file(filePath)
+		data = []
+		File.open(filePath, "r") do |f|
 		  f.each_line do |line|
-		    puts line
-		    @data.push(line.chomp)
+		    data.push(line.upcase.chomp)
 		  end
 		end
+		return data
 	end
 
-	def run_command
-		@data.each do |command|
-			if command =~ /^PLACE */
-				command.sub!("PLACE ","")
-				command_arr = command.split(",")
-				@controller.place_robot(command_arr[0].to_i,command_arr[1].to_i,direction_translate(command_arr[2]))
-			elsif command =~ /^MOVE$/
-				@controller.move
-			elsif command =~ /^LEFT$/
-				@controller.turn("left")
-			elsif command =~ /^RIGHT$/
-				@controller.turn("right")
-			elsif command =~ /^REPORT$/
-				place = @controller.show_place
-				puts "#{place[:x]},#{place[:y]},#{place[:dir]}"
+	def get_data
+		puts "Please input file path"
+		filePath = gets.chomp
+		data = read_file(filePath)
+
+		data.map! do |command|
+			if is_place?(command) || is_move?(command) || is_left?(command) || 
+				is_right?(command) || is_report?(command)
+				command
+			else
+				nil
 			end
 		end
+
+		data.compact!
+
+		return data
 	end
 
-	def direction_translate(direction)
-		if direction.upcase == "NORTH"
-			return 0
-		elsif direction.upcase == "EAST"
-			return 90
-		elsif direction.upcase == "SOUTH"
-			return 180
-		elsif direction.upcase == "WEST"
-			return 270
-		else
-			return "error"
-		end
+	def is_place?(command)
+		!!(command =~ /^PLACE [0-9]+,[0-9]+,(NORTH|SOUTH|EAST|WEST)$/)
+	end
+
+	def is_move?(command)
+		!!(command =~ /^MOVE$/)
+	end
+
+	def is_left?(command)
+		!!(command =~ /^LEFT$/)
+	end
+
+	def is_right?(command)
+		!!(command =~ /^RIGHT$/)
+	end
+
+	def is_report?(command)
+		!!(command =~ /^REPORT$/)
+	end
+	
+	def report(data_object)
+		puts "#{data_object[:x]},#{data_object[:y]},#{data_object[:dir]}"
 	end
 end
+
 binding.pry
