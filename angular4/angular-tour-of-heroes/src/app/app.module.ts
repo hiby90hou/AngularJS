06f422f1c1c2d3f0a573606acd658252a2fc3905
hiby90hou <hiby.90hou@qq.com>
2018-03-23 18:01:21 +1100

    add route practice

--- angular4/angular-tour-of-heroes/src/app/app.module.ts
+++ angular4/angular-tour-of-heroes/src/app/app.module.ts
@@ -1,22 +1,37 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
-
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { HeroService } from './hero.service';
+import { MessageService } from './message.service';
+import { MessagesComponent } from './messages/messages.component';
 
+import { AppRoutingModule } from './app-routing.module';
+
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeroesComponent
-  ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule,
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false }
+    )
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    MessagesComponent
+  ],
+  providers: [ HeroService, MessageService ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
--- angular4/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular4/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,20 +1,51 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from '../hero';
+// import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-export class HeroesComponent implements OnInit {
-  // hero = 'Windstorm';
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+// export class HeroesComponent implements OnInit {
+//   // hero = 'Windstorm';
+//   // hero: Hero = {
+//   //   id: 1,
+//   //   name: 'Windstorm'
+//   // };
+//   // heroes = HEROES;
+//   heroes: Hero[];
+//   selectedHero: Hero;
 
-  constructor() { }
+//   onSelect(hero: Hero): void {
+//     this.selectedHero = hero;
+//     console.log(this.selectedHero);
+//   }
+
+//   getHeroes(): void {
+//     this.heroService.getHeroes()
+//         .subscribe(heroes => this.heroes = heroes);
+//   }
+
+//   constructor(private heroService: HeroService) { }
+
+//   ngOnInit() {
+//     this.getHeroes();
+//   }
+// }
+
+export class HeroesComponent implements OnInit {
+  heroes: Hero[];
+
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+    .subscribe(heroes => this.heroes = heroes);
   }
 }
