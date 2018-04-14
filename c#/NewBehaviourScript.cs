7b36ece99db54cf6e82a9c214a86149925474bad
hiby90hou <hiby.90hou@qq.com>
2018-04-14 19:46:14 +1000

    learn unity

---
+++ c#/NewBehaviourScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.SerializableAttribute]
+public class customerStruct {
+    public List<string> a;
+    public Color c;
+    public AnimationCurve curve;
+    public Gradient g;
+}
+
+public class NewBehaviourScript : MonoBehaviour {
+    public List<string> a;
+    public Color c;
+    public AnimationCurve curve;
+    public Gradient g;
+    public customerStruct cus;
+
+    // stop other people in unity see variable
+    [HideInInspector]
+    public int bb;
+
+    // only the people in unity can access this value other class cannot
+    [SerializeField]
+    private bool pp;
+
+    // Use this for initialization
+    void Start () {
+        Debug.Log(a.Count);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log("Hello world");
+        GameObject.FindWithTag("npc");
+	}
+}
